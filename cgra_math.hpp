af8fabdcc97144f98f5c514f9d976e8d1ecb30b3
JJScott <joshua.scott.132@gmail.com>
2016-12-20 16:13:40 +1300

    Initial hello world commit

---
+++ cgra_math.hpp
@@ -0,0 +1,40 @@
+//----------------------------------------------------------------------------
+// 
+// CGRA Math Library
+//
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+
+
+
+
+
+namespace cgra {
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
---
+++ tests/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include <cgra_math.hpp>
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+}
